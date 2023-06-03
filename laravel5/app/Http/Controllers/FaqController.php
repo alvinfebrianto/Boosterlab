1a9320943527a6f84ddc9c9ed2b165149f41b58b
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2023-06-04 00:07:11 +0700

    -

---
+++ laravel5/app/Http/Controllers/FaqController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FaqController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('faq');
+    }
+
+    public function adminFAQ()
+    {
+        return view('admin.faq');
+    }
+}
