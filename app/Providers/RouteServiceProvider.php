b5051db534f9af224e96e49f98be8a237bfba1bc
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2026-05-25 22:18:56 +0700

    build(deps): upgrade to Laravel 12 and update dependencies
    Upgraded Laravel framework to v12.60.2, PHP to ^8.2, and updated all dependencies for compatibility. Applied necessary refactors to Middleware, RouteServiceProvider, and test configurations.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,7 +19,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
-    protected $namespace = 'App\Http\Controllers';
     /**
      * Define your route model bindings, pattern filters, etc.
      *
@@ -31,11 +30,9 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('web')
-                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
 
             Route::prefix('api')
-                ->namespace($this->namespace)
                 ->middleware('api')
                 ->group(base_path('routes/api.php'));
         });
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnakController;
 use App\Http\Controllers\ArtikelController;
