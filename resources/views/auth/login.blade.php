d96f47a4fa7b35f8c2788ac5ef39734688314693
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2026-05-26 15:00:33 +0700

    refactor(auth): remove forgot password link from login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,11 +46,6 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Lupa Password?') }}
-                                    </a>
-                                @endif
                             </div>
                         </div>
                     </form>
